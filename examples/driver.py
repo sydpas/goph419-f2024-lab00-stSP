129b84353b7831cbac38e957c35d0e260beb5d05
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2024-09-11 11:30:02 -0600

    added examples script

---
+++ examples/driver.py
@@ -0,0 +1,12 @@
+from my_python_package.operators import add, multiply
+
+
+def main():
+    a = 2
+    b = 5
+    print(add(a, b))
+    print(multiply(a, b))
+
+
+if __name__ == "__main__":
+    main()
