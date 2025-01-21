6cd678b6509c306a680ecb263a654097904be847
Sydney Pasloski <sydney.pasloski@ucalgary.ca>
2025-01-21 09:29:00 -0700

    added code so terminal can find src direct path

--- examples/driver.py
+++ examples/driver.py
@@ -1,11 +1,22 @@
-from my_python_package.operators import add, multiply
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# this is to run python examples/driver.py in the terminal
+# python can't find src/functions/ because examples/ isn't in the main search path
+# this tells python where to look for src
+
+
+from src.functions.operators import (
+    add,
+    multiply,
+)
 
 
 def main():
     a = 2
     b = 5
-    print(add(a, b))
-    print(multiply(a, b))
+    print(f'Addition: {add(a, b)}')
+    print(f'Multiply: {multiply(a, b)}')
 
 
 if __name__ == "__main__":
